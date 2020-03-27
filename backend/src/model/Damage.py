d5caa0849cfcbd2e2525f8d02810150679ee8eaf
Subarna Chowdhury Soma <subarna089@gmail.com>
2020-03-27 12:23:08 -0700

    removed python

--- backend/src/model/Damage.py
+++
@@ -1,9 +0,0 @@
-class Damage:
-    # attributes:
-    _id = 0
-    damageType = ''
-    vehicleTypeId = 0
-
-    # constructor
-    def __init__(self):
-        self._id = 0
--- backend/src/model/VehicleType.py
+++
@@ -1,9 +0,0 @@
-class VehicleType:
-    # attributes:
-    _id = 0
-    vehicleSize = ''
-    vehicleClass = ''
-
-   #constructor
-    def __init__(self):
-        self._id = 0
