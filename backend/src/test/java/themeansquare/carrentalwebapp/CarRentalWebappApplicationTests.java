7100bf0afa3c08970439fd0be6c4bf3114252910
Subarna Chowdhury Soma <subarna089@gmail.com>
2020-03-27 15:04:26 -0700

    spring setup with web dependencies

--- backend/src/test/java/themeansquare/carrentalwebapp/CarRentalWebappApplicationTests.java
+++ backend/src/test/java/themeansquare/carrentalwebapp/CarRentalWebappApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.carrent;
+package themeansquare.carrentalwebapp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class CarRentApplicationTests {
+class CarRentalWebappApplicationTests {
 
 	@Test
 	void contextLoads() {
