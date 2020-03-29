3e524421cab6e75475c09753d4780778104e7ddb
Wasae Qureshi <wasaequreshi@gmail.com>
2020-03-29 07:51:41 -0700

    Was able to import model into controller

--- backend/src/main/java/themeansquare/controller/RegistrationController.java
+++ backend/src/main/java/themeansquare/controller/RegistrationController.java
@@ -1,5 +1,6 @@
 package themeansquare.controller;
 
+import themeansquare.service.Registration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,7 @@ public class RegistrationController {
 
 	@GetMapping("/registration")
     public String hello(@RequestParam(value = "name", defaultValue = "World") String name, @RequestParam(value = "test", defaultValue = "test") String test) {
+        Registration reg = new Registration();
         return String.format("Hello %s!", test);
 	}
 
