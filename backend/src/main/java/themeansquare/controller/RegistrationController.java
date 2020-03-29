8a9f823792e0a9294c4991e141ba1c758878bdbc
Subarna Chowdhury Soma <subarna089@gmail.com>
2020-03-29 15:48:46 -0700

    made changes to structur and added IRegistration file

--- backend/src/main/java/themeansquare/controller/RegistrationController.java
+++ backend/src/main/java/themeansquare/controller/RegistrationController.java
@@ -10,7 +10,7 @@ public class RegistrationController {
 
 	@GetMapping("/registration")
     public String hello(@RequestParam(value = "name", defaultValue = "World") String name, @RequestParam(value = "test", defaultValue = "test") String test) {
-        Registration reg = new Registration();
+        Registration reg = new Registration(); // call interface not class 
         return String.format("Hello %s!", test);
 	}
 
