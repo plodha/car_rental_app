c2dff9154d3c5cf1a55749c6a5dcdee4fb0943cf
Pranav Lodha <lodha.pranav2014@gmail.com>
2020-04-05 15:34:16 -0700

    getting one entity up and running

--- backend/src/main/java/themeansquare/carrentalservice/CarRentalServiceApplication.java
+++ backend/src/main/java/themeansquare/carrentalservice/CarRentalServiceApplication.java
@@ -2,10 +2,12 @@ package themeansquare.carrentalservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = {"themeansquare.controller"})
-@RestController
+@EnableJpaRepositories("themeansquare.repository")
+@EntityScan("themeansquare.model")
 public class CarRentalServiceApplication {
 
 	public static void main(String[] args) {
--- backend/src/main/java/themeansquare/model/Address.java
+++ backend/src/main/java/themeansquare/model/Address.java
@@ -1,13 +1,45 @@
 package themeansquare.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Address {
 
-    int _id;
-    String street, city, state, zipcode;
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private int Id;
+    
+    private String Street;
+    
+    private String City;
+    
+    private String State;
+    
+    private int ZipCode;
+    
+    // Getters and Setters
+    
+    public int getId() { return Id; }
 
-    public Address()
-    {
-        
-    }
+	public void setId(int id) { Id = id; }
+
+	public String getStreet() { return Street; }
+
+	public void setStreet(String street) {Street = street; }
+
+	public String getCity() { return City; }
+
+	public void setCity(String city) { City = city; }
+
+	public String getState() { return State; }
+
+	public void setState(String state) {State = state; }
+
+	public int getZipCode() { return ZipCode; }
+
+	public void setZipCode(int zipCode) { ZipCode = zipCode; }
+	
 }
-
