6d3863c9ed5e41399f5f5dcaa6d336d6a25e93c5
Wasae Qureshi <wasaequreshi@gmail.com>
2020-05-04 08:56:55 -0700

    fix for delete/puts cors

--- backend/src/main/java/themeansquare/carrentalservice/CarRentalServiceApplication.java
+++ backend/src/main/java/themeansquare/carrentalservice/CarRentalServiceApplication.java
@@ -24,7 +24,7 @@ public class CarRentalServiceApplication {
 			return new WebMvcConfigurer() {
 					@Override
 					public void addCorsMappings(CorsRegistry registry) {
-							registry.addMapping("/**").allowedOrigins("*");
+							registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST","PUT", "DELETE");
 					}
 			};
 	}
